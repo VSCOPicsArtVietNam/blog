df320ded9251307c8e955650b4468c4d6a20333e
VSCOPicsArtVietNam <30444267+VSCOPicsArtVietNam@users.noreply.github.com>
2017-12-09 00:10:59 +0700

    Update notification.js

--- uch/notification.js
+++ uch/notification.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-var html ='<p class="content">Công cụ này chỉ hỗ trợ thao tác mua UCH nhanh hơn chứ không thế quyết định bạn có mua thành công hay không. Cái này hoàn toàn phụ thuộc vào may mắn của các bạn.</p>'+
+var html ='<p class="content">Hôm nay ngày 8/12 là đợt bán ICO cuối cùng cho thành viên UCH. Đợt ICO ngày 10/12 sẽ chỉ bán cho riêng Leader</p>'+
 '<p class="content">Công cụ đã được sửa đổi mã giúp tối ưu hơn phiên bản gốc và kiểm tra kĩ và không phát hiện mã độc.</p>'+
 '<p class="content">Để được hỗ trợ về UCH hãy đăng ký <a style="color: #0769ad" href="https://ucoincash.co/ref/mrhuy92" target="_blank">tại đây</a></p>'+
 '<p class="link"><a style="color: #0769ad" href="https://ucoincash.co/Account/Signin" target="_blank">ĐĂNG NHẬP</a> - <a style="color: #0769ad" href="https://ucoincash.co/ICO/Buy" target="_blank">MUA UCH</a></p>'+
