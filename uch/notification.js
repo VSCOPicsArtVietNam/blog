ecde4823bcd9234a29f581414ca3d8f325e1392a
VSCOPicsArtVietNam <30444267+VSCOPicsArtVietNam@users.noreply.github.com>
2017-12-08 12:50:13 +0700

    Create notification.js

---
+++ uch/notification.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+var html ='<p class="content">Công cụ này chỉ hỗ trợ thao tác mua UCH nhanh hơn chứ không thế quyết định bạn có mua thành công hay không. Cái này hoàn toàn phụ thuộc vào may mắn của các bạn.</p>'+
+'<p class="content">Công cụ đã được sửa đổi mã giúp tối ưu hơn phiên bản gốc và kiểm tra kĩ và không phát hiện mã độc.</p>'+
+'<p class="content">Để được hỗ trợ về UCH hãy đăng ký <a style="color: #0769ad" href="https://ucoincash.co/ref/mrhuy92" target="_blank">tại đây</a></p>'+
+'<p class="link"><a style="color: #0769ad" href="https://ucoincash.co/Account/Signin" target="_blank">ĐĂNG NHẬP</a> - <a style="color: #0769ad" href="https://ucoincash.co/ICO/Buy" target="_blank">MUA UCH</a></p>'+
+'<p class="author">Version 1.4 edit by <a style="color: #0769ad" href="https://www.facebook.com/mr.huy.92" target="_blank">mr.huy.92</a></p>';
+$(".panel").append(html);
+});
