7651bbd72cb1ec20a7a9bef7efdaf28ef9c5a8e7
VSCOPicsArtVietNam <30444267+VSCOPicsArtVietNam@users.noreply.github.com>
2017-12-08 23:53:22 +0700

    Update clock.js

--- uch/clock.js
+++ uch/clock.js
@@ -6,19 +6,25 @@ $(document).ready(function() {
 	var currentDate = new Date();
 	
 	// Set some date in the future. In this case, it's always Jan 1
-	var futureDate = new Date(1512918000000);
+	var futureDate = new Date(1512751991592);
 	
 	// Calculate the difference in seconds between the future and current date
 	var diffTime = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	
 	// Instantiate a coutdown FlipClock
-	clock = $('.clock').FlipClock(diffTime , {
+	var clock = $('.clock').FlipClock(diffTime , {
 		clockFace: 'DailyCounter',
 		language:'vi-vn',
 		autoStart: false,
                 callbacks: {
                     stop: function () {
-                      
+                      var html = [];
+                        html.push('<h4 class="buy-ico-title-time"><img class="img-logo" src="../images/time-buy-ico.png"><span>ICO START NOW</span></h4>');
+                        $('#div-time-count-down').html(html.join(''));
                     }
 	});
+	
+        clock.setTime(diffTime);
+        clock.setCountdown(true);
+        clock.start();
 });
