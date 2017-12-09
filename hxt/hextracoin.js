12ef62a7d06dedfa4844f70f7f623615f2fa4fa7
VSCOPicsArtVietNam <30444267+VSCOPicsArtVietNam@users.noreply.github.com>
2017-12-09 07:50:58 +0700

    Update hextracoin.js

--- hxt/hextracoin.js
+++ hxt/hextracoin.js
@@ -13,6 +13,38 @@ $(function() {
 		//e.preventDefault();
 	});
 }); 
+
+(function getUSDtoVND(){
+	$.ajax({
+		type: 'GET',
+		url: 'https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx',
+		dataType: "xml",
+		success: function (data) {
+			$(data).find('Exrate').each(function(){
+				var $entry = $(this);
+				$entry.attr("CurrencyCode") === "USD" ? coinCurrency[altCurrency].Price = $entry.attr("Sell") : coinCurrency[altCurrency].Price = 0;
+			})
+			var html = [];
+			html.push('<p class="info-section">');
+			html.push('<span class="txt-coin-Exchange">' + defaultCurrency + '</span>');
+			html.push('<span class="txt-coin-ExchangeTo">' + altCurrency +'</span>');
+			html.push('</p>');
+			html.push('<p class="price-section">');
+			html.push('<span class="txt-coin-price">' + $.number(1, 0) + '</span>');
+			html.push('<span class="txt-coin-currency">' + coinCurrency[defaultCurrency].CurrencyNameSymbol +'</span>');
+			html.push('<span class="txt-coin-pricevnd">' + $.number(coinCurrency[altCurrency].Price, 0) +'</span>');
+			html.push('<span class="txt-coin-currency">' + coinCurrency[altCurrency].CurrencyNameSymbol +'</span>');
+			html.push('</p>');
+			$('#vnd-price').html(html.join(' '));
+		},
+		complete: function(data) {
+			convertHXTtoBTC();
+			setTimeout(getUSDtoVND, seconds * 1000);
+		},
+		error: function() { console.log('Có lỗi!'); }
+	});
+})();
+
 var seconds = 60; // Thời gian làm mới giá
 (function getHXTPrice(){
 	$.ajax({
@@ -78,37 +110,6 @@ var seconds = 60; // Thời gian làm mới giá
 	});
 })();
 
-(function getUSDtoVND(){
-	$.ajax({
-		type: 'GET',
-		url: 'https://www.vietcombank.com.vn/ExchangeRates/ExrateXML.aspx',
-		dataType: "xml",
-		success: function (data) {
-			$(data).find('Exrate').each(function(){
-				var $entry = $(this);
-				$entry.attr("CurrencyCode") === "USD" ? coinCurrency[altCurrency].Price = $entry.attr("Sell") : coinCurrency[altCurrency].Price = 0;
-			})
-			var html = [];
-			html.push('<p class="info-section">');
-			html.push('<span class="txt-coin-Exchange">' + defaultCurrency + '</span>');
-			html.push('<span class="txt-coin-ExchangeTo">' + altCurrency +'</span>');
-			html.push('</p>');
-			html.push('<p class="price-section">');
-			html.push('<span class="txt-coin-price">' + $.number(1, 0) + '</span>');
-			html.push('<span class="txt-coin-currency">' + coinCurrency[defaultCurrency].CurrencyNameSymbol +'</span>');
-			html.push('<span class="txt-coin-pricevnd">' + $.number(coinCurrency[altCurrency].Price, 0) +'</span>');
-			html.push('<span class="txt-coin-currency">' + coinCurrency[altCurrency].CurrencyNameSymbol +'</span>');
-			html.push('</p>');
-			$('#vnd-price').html(html.join(' '));
-		},
-		complete: function(data) {
-			convertHXTtoBTC();
-			setTimeout(getUSDtoVND, seconds * 1000);
-		},
-		error: function() { console.log('Có lỗi!'); }
-	});
-})();
-
 $(document).ready(function(){
 	//var autoConvert = setInterval(convertHXTtoBTC(), 1000);
 });
@@ -134,18 +135,18 @@ function doConvert(t, n) {
 	switch(t){
 		case "hxt-btc":
 		r = parseFloat(coinCurrency.HXT.Price / coinCurrency.BTC.Price).toFixed(8);
-	r = $.number(r, 8);
-	return r;
-	case "hxt-vnd":	
-	r = parseFloat(n * coinCurrency[altCurrency].Price).toFixed(2);
-	r = $.number(r, 0);
-	return r;
-	case "btc-vnd":
-	console.log("BTC " + n);
-	r = parseFloat(n * coinCurrency[altCurrency].Price).toFixed(2);
-	r = $.number(r, 0);
-	return r;
-	default:
-	return "Default";
-}
+		r = $.number(r, 8);
+		return r;
+		case "hxt-vnd":	
+		r = parseFloat(n * coinCurrency[altCurrency].Price).toFixed(2);
+		r = $.number(r, 0);
+		return r;
+		case "btc-vnd":
+		console.log("BTC " + n);
+		r = parseFloat(n * coinCurrency[altCurrency].Price).toFixed(2);
+		r = $.number(r, 0);
+		return r;
+		default:
+		return "Default";
+	}
 }
